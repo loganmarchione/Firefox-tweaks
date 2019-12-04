834c94fff311e8edba96b21dfb1ba49e164b32f4
Logan Marchione <loganmarchione@users.noreply.github.com>
2019-12-04 13:42:11 -0500

    Update user.js

--- user.js
+++ user.js
@@ -32,62 +32,74 @@ user_pref("browser.fixup.alternate.enabled", false);
 // Don't trim HTTP/HTTPS off of URLs in the address bar
 user_pref("browser.urlbar.trimURLs", false);
 
+// Display "Not Secure" icon and text on HTTP websites
+user_pref("security.insecure_connection_icon.enabled", true);
+user_pref("security.insecure_connection_text.enabled", true);
+
 ////////////////////////////////////////////////////
-//   Reading and content
+//   User experience (UX) and user interface (UI)
 ////////////////////////////////////////////////////
 
 // When double-clicking a word on a page, only copy the word itself, not the space character next to it 
 user_pref("layout.word_select.eat_space_to_next_word", false);
 
 // Settings for finding
-user_pref("findbar.highlightAll", true);
-user_pref("findbar.modalHighlight", true);
+user_pref("findbar.highlightAll", true);    // Highlight all words on that page
+user_pref("findbar.modalHighlight", true);  // Dim the rest of the page
 
 // Enable spellchecker on all text fields
 user_pref("layout.spellcheckDefault", 2);
+
+// Backspace key goes back a page
+user_pref("browser.backspace_action", 0);
+
+// Put tabs in the title bar (saves space)
+user_pref("browser.tabs.drawInTitlebar", true);
+
+// Enable loading userChrome.css and userContent.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 ////////////////////////////////////////////////////
 //   Startup and new pages/tabs/windows
 ////////////////////////////////////////////////////
 
-user_pref("browser.startup.page", 0);                 // Set start page
-user_pref("browser.startup.homepage", "about:blank")  // Set "Homepage and new windows"
-user_pref("browser.newtabpage.enabled", false);       // Set "New tabs"
+user_pref("browser.shell.checkDefaultBrowser", false); // Disable check for default browser
+user_pref("browser.startup.page", 0);                  // Set start page
+user_pref("browser.startup.homepage", "about:blank")   // Set "Homepage and new windows"
+user_pref("browser.newtabpage.enabled", false);        // Set "New tabs"
 user_pref("browser.newtab.preload", false);
-user_pref("browser.onboarding.enabled", false);       // Hide onboarding tour (uses Google Analytics)
+user_pref("browser.onboarding.enabled", false);        // Hide onboarding tour (uses Google Analytics)
 
 ////////////////////////////////////////////////////
 //   Activity Stream
 ////////////////////////////////////////////////////
 
-// Set new tab page to a blank page
-user_pref("browser.newtabpage.activity-stream.enabled", false);
 // Disable Activity Stream recent Highlights in the Library
 user_pref("browser.library.activity-stream.enabled", false);
-// Disable Activity Stream telemetry
-user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
-user_pref("browser.newtabpage.activity-stream.telemetry", false);
-user_pref("browser.newtabpage.activity-stream.telemetry.ping.endpoint", "");
-// Disable Activity Stream Web Search
-user_pref("browser.newtabpage.activity-stream.showSearch", false);
-// Disable Activity Stream Top Sites
-user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
-// Disable Activity Stream Pocket content
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-user_pref("browser.newtabpage.activity-stream.showSponsored", false);
-// Disable Activity Stream Highlights
 user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
-// Disable Activity Stream Snippets
-user_pref("browser.newtabpage.activity-stream.disableSnippets", true);
+
+// Disable recommdations
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);   // Disable "Recommend extensions as you browse"
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false); // Disable "Recommend features as you browse"
+
+// Disable Activity Stream telemetry
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry.ping.endpoint", "");
+
+// Disable Activity Stream Snippets (runs code from a remote server)
 user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
-// Disable recommend add-ons as you browse
-user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
-// Disable recommend features as your browse
-user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.providers.snippets", "");
+
+// Disable other stuff
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 
 ////////////////////////////////////////////////////
 //   Privacy
@@ -113,9 +125,6 @@ user_pref("app.normandy.api_url", "");
 
 // Disable "Upload" feature on Screenshots
 user_pref("extensions.screenshots.upload-disabled", true);
-
-// Disable Flash
-user_pref("plugin.state.flash", 0);
 
 // Disable creating thumbnails from each page
 user_pref("browser.pagethumbnails.capturing_disabled", true); // [HIDDEN PREF]
@@ -217,34 +226,15 @@ user_pref("browser.cache.disk_cache_ssl", false);
 // Disable Pocket
 user_pref("extensions.pocket.enabled", false);
 
-// Backspace key goes back a page
-user_pref("browser.backspace_action", 0);
-
-// Disable check for default browser
-user_pref("browser.shell.checkDefaultBrowser", false);
-
 // Disable notifications
 user_pref("dom.webnotifications.enabled", false);
 user_pref("dom.webnotifications.serviceworker.enabled", false);
 
-// Enable loading userChrome.css and userContent.css
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-
-// Make fullscreen warning go away
-user_pref("full-screen-api.warning.timeout", 0);
+// Don't warn when opening about:config 
+user_pref("browser.aboutConfig.showWarning", false);  // for the new HTML version [FF71+]
 
 // Enable WebM
 user_pref("media.mediasource.webm.enabled", true);
 
-// Don't warn when opening about:config
-user_pref("browser.aboutConfig.showWarning", false); // for the new HTML version [FF71+]
-
-// Display "Not Secure" icon and text on HTTP websites
-user_pref("security.insecure_connection_icon.enabled", true);
-user_pref("security.insecure_connection_text.enabled", true);
-
-// Enable click to play for plugins
-user_pref("plugins.click_to_play", true);
-
-// Put tabs in the title bar (saves space)
-user_pref("browser.tabs.drawInTitlebar", true);
+// Disable Flash
+user_pref("plugin.state.flash", 0);
