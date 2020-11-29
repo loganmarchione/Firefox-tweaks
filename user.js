77778bb0e86f7b8116510bfddaf0034bd39aa7a6
Logan Marchione <loganmarchione@users.noreply.github.com>
2020-11-29 20:43:16 +0000

    Enable HTTPS-only mode

--- user.js
+++ user.js
@@ -143,6 +143,9 @@ user_pref("media.navigator.video.enabled", false);
 // Disable DNS-over-HTTPS because I want to use my internal DNS resolver
 user_pref("network.trr.mode", 5);
 
+// Enable HTTPS-only mode
+user_pref("dom.security.https_only_mode", true);
+
 // Make sure DNS is sent through the proxy server when using a SOCKS v5 proxy
 user_pref("network.proxy.socks_remote_dns", true);
 
